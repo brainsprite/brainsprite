83feed8b0d53e2a94c27826107b844309f9163b4
George Peristerakis <peristerakis@gmail.com>
2017-03-06 14:24:13 -0500

    Added the necessary files to make brainspite a python package

---
+++ setup.py
@@ -0,0 +1,36 @@
+from codecs import open
+from setuptools import setup
+
+
+with open('README.md', encoding='utf-8') as fd:
+    LONG_DESCRIPTION = fd.read()
+
+setup(
+    name='brainsprite',
+    long_description=LONG_DESCRIPTION,
+    license='MIT',
+    packages=['brainsprite'],
+    package_data={
+        'assets': 'assets/*',
+        'examples': 'examples/*'
+    },
+    include_package_data=True,
+    install_requires=[
+        'matplotlib',
+        'PILLOW',
+        'numpy',
+        'nibabel',
+        'nilearn',
+        'scipy'
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
