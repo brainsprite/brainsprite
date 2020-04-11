c43a2be40a3f901f10894ffafe22700f6e72eaf2
pbellec <pierre.bellec@gmail.com>
2020-04-11 15:20:37 -0400

    First successful test with jest!! now on to puppeteer.

--- tests/setup.js
+++ tests/setup.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 module.exports = async function() {
   const app = express();
-  app.use(express.static('public/'));
+  app.use(express.static(__dirname + '/'));
   global.__SERVER__ = app.listen(8080);
 };
