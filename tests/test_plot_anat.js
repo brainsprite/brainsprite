57c3419f52e2f295fc1279fc702217c9a90d351e
pbellec <pierre.bellec@gmail.com>
2020-04-12 09:20:58 -0400

    There is now a reusable template for tests, and an environment variable to re-initialize test results.

--- tests/test_plot_anat.js
+++ tests/test_plot_anat.js
@@ -1,34 +1,6 @@
-const path = require('path');
-const http = require('http');
-const file = 'plot_anat.html'
+const template = require('./template.js');
 
-describe('index page', () => {
-  let page;
-
-  beforeAll(async () => {
-    page = await __BROWSER__.newPage();
-    await page.goto('http://localhost:8080/' + file);
-    await page.screenshot({
-      clip: { x: 0, y: 0, width: 800, height: 340 },
-      path: `${path.resolve(__dirname)}/plot_anat.png`,
-    });
-  }, 5000);
-
-  afterAll(async () => {
-    await page.close();
-  });
-
-  it(
-    'loads the index page HTTP',
-    (done) => {
-      http.request({
-        hostname: 'localhost',
-        port: 8080,
-        path: '/plot_anat.html'
-    },
-    (response) => {
-      expect(response.statusCode).toBe(200);
-      done();
-    }).end();
-  });
-});
+file = 'plot_anat.html'
+template.fullTest(file,
+  { x: 0, y: 0, width: 800, height: 340 }
+)
