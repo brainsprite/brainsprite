7364523560e3cdb04e6de4ab92cf1f788c461e5d
pbellec <pierre.bellec@gmail.com>
2020-04-14 22:56:06 -0400

    Trying to have a full test suite run through circleci.

--- docs/update_docs.sh
+++ docs/update_docs.sh
@@ -4,9 +4,6 @@ rm -rf build
 rm -rf source/auto_examples
 rm -rf ../tests/plot_*.html
 
-echo Updating brainsprite.js as a docs static file
-cp ../brainsprite.js source/_static
-
 echo Building docs
 make html
 
