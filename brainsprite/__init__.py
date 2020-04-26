98f16eaa51906ac943dd6b3299ef16a11f3c3468
pbellec <pierre.bellec@gmail.com>
2020-04-26 14:57:53 -0400

    Stand-alone python API for brainsprite.

---
+++ brainsprite/__init__.py
@@ -0,0 +1,4 @@
+"""Brainsprite python API."""
+from .brainsprite import brainsprite
+from brainsprite import viewer, build_sprite, build_snippet
+__all__ = ['view_brain', 'build_sprite', 'build_snippet']
