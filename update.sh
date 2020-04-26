d790c9ce2dfb13c87d3e51d3ed9c42f88528ee9e
pbellec <pierre.bellec@gmail.com>
2020-04-26 14:49:16 -0400

    Added updating assets, and moved the update script to the root.

---
+++ update.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+CURRPATH=`pwd`
+SCRIPT=`realpath -s $0`
+SCRIPTPATH=`dirname $SCRIPT`
+
+cd $SCRIPTPATH
+
+echo minifying brainsprite.js
+minify brainsprite.js > brainsprite.min.js
+
+echo updating js assets in brainsprite.py
+cp brainsprite.min.js brainsprite/assets/js/
+cp assets/jquery*/jquery.min.js brainsprite/assets/js
+
+echo flushing previous docs build
+rm -rf $SCRIPTPATH/docs/build
+rm -rf $SCRIPTPATH/docs/source/auto_examples
+rm -rf $SCRIPTPATH/tests/plot_*.html
+
+echo Building docs
+make html
+
+echo Running tests and updating tutorial thumbnails
+npm test
+
+cd $CURRPATH
